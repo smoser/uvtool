28e508395aa376d7e2e7969828c298031c4b2f07
Scott Moser <smoser@brickies.net>
2014-12-04 17:13:04 -0500

    remote-wait.sh: do not wait for runlevel 2

--- remote-wait.sh
+++ remote-wait.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 set -e
 
+## Not waiting for runlevel 2 as this seems to work still
+## on trusty, and snappy shows runlevel 5. Admittedly, don't know what
+## problem this solved.
 # Wait for runlevel 2
-while [ "$(runlevel|awk '{print $2}')" != 2 ]; do sleep $UVTOOL_WAIT_INTERVAL; done
+#while [ "$(runlevel|awk '{print $2}')" != 2 ]; do sleep $UVTOOL_WAIT_INTERVAL; done
 
 # Wait for cloud-init's signal
 while [ ! -e /var/lib/cloud/instance/boot-finished ]; do sleep $UVTOOL_WAIT_INTERVAL; done
